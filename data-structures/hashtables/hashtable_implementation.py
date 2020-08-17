f72c8e6777d86c348f300ce14b0267d6d2199e3d
Salmane Tamo <tamosalmane@gmail.com>
2020-08-17 22:34:16 +0400

    Hahstable implementation complete

--- data-structures/hashtables/hashtable_implementation.py
+++ data-structures/hashtables/hashtable_implementation.py
@@ -4,9 +4,12 @@ class HashTable:
         self.values = [None] * m
         self.keys = [None] * m
 
-    def hash(self, key, m):
+    def hash(self, key, size):
         key_as_int = sum([ord(char) for char in key])
-        return key_as_int % m
+        return key_as_int % size
+
+    def rehash(self, old_hash, size):
+        return (old_hash + 1) % size
 
     def add(self, key, value):
         hash = self.hash(key, self.m)
@@ -17,24 +20,49 @@ class HashTable:
             if self.keys[hash] == key:
                 self.values[hash] = value
             else:
-                new_hash = self.hash(hash, self.m + 1)
-                while not self.keys[new_hash] and self.keys[new_hash] != key:
-                    new_hash = self.hash(new_hash, self.m + 1)
+                new_hash = self.rehash(hash, self.m)
+                while self.keys[new_hash] and self.keys[new_hash] != key:
+                    new_hash = self.rehash(new_hash, self.m)
 
                 self.keys[new_hash] = key
                 self.values[new_hash] = value
 
-    # def exists(self, key):
-    #     return self.hash(key) < self.m
-    #
-    # def get(self, key):
-    #     if not self.exists(key):
-    #         raise KeyError('Key not found')
-    #     hash = self.hash(key, self.m)
-    #     return self.values[hash]
-    #
-    # def remove(self, key):
-    #     if not self.exists(key):
-    #         raise KeyError('Key not found')
-    #     hash = self.hash(key, self.m)
-    #     self.values[hash] = None
+    def exists(self, key):
+        hash = self.hash(key, self.m)
+        return hash < len(self.keys) and self.keys[hash] is not None
+
+    def get(self, key):
+        start_hash = self.hash(key, self.m)
+        value = None
+        found = False
+        stop = False
+        current_hash = start_hash
+
+        while self.keys[current_hash] is not None and not found and not stop:
+            if self.keys[current_hash] == key:
+                found = True
+                value = self.values[current_hash]
+            else:
+                current_hash = self.rehash(current_hash, self.m)
+                if current_hash == start_hash:
+                    stop = True
+        return value
+
+    def remove(self, key):
+        if not self.exists(key):
+            raise KeyError('Key not found')
+
+        start_hash = self.hash(key, self.m)
+        found = False
+        stop = False
+        current_hash = start_hash
+
+        while self.keys[current_hash] is not None and not found and not stop:
+            if self.keys[current_hash] == key:
+                self.keys[current_hash] = None
+                self.values[current_hash] = None
+                found = True
+            else:
+                current_hash = self.rehash(current_hash, self.m)
+                if current_hash == start_hash:
+                    stop = True
